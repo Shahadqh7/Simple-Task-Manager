75b0dccc1a77de427e1b726ff3ec3dc00da81e5d
shahadqh7 <shahadalsadd@gmail.com>
2024-03-22 00:48:34 +0300

    .

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express') // Extract express library
+const app = express() //Create an instance
+app.use(express.json())
+
+const TaskRoute= require('./routes/task')
+const controller = require('./controlleer/taskController' )
+
+app.use('/task' , TaskRoute)
+app.listen(4000, console.log('listening'))
---
+++ controlleer/taskController.js
@@ -0,0 +1,109 @@
+const fs = require('fs')
+const uuid = require('uuid')
+const path = require('path');
+
+function readFile() {
+    const filePath = path.resolve(__dirname, '../Task.json');
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading file:', error);
+        return null;
+    }
+
+    
+}
+function writeFile(data) {
+    // Writing the data to University.json file
+    fs.writeFileSync(`${__dirname}/../Task.json`, JSON.stringify(data), 'utf8')
+}
+
+     const getAll = (req , res) => {
+    
+        try {
+            // Sending a JSON response containing all universities
+            res.status(200).json({task: readFile()})
+        } catch(e) {
+            // Handling errors and sending a 500 status code with error message
+            res.status(500).json({ error: e })
+        }
+    } 
+
+    const getById = (req ,res) =>{
+        
+        try{
+            const task_id = req.params.id
+            const task = readFile()[task_id]
+        
+            if (task) {
+           return res.status(200).json({Task :task })
+        }
+         res.status(404).json({'message':`Task id ${task_id} dose not exist`});
+    } catch(e) {
+        // Handling errors and sending a 500 status code with error message
+        res.status(500).json({ error: e })
+    }
+    }
+
+    const Create = (req , res) => {
+        const data = req.body
+        let task = readFile()
+        const id = uuid.v4()
+    
+        if(task[id]){
+             return res.json({'message' :`Task Id: ${id} is already exist`})
+    
+        }
+        data['id']=id
+        task[id] = data
+        writeFile(task)
+    
+         res.status(201).json({'message' :task[id]})
+
+   
+    
+    }
+
+    const update = (req , res ) =>{
+        const data = req.body
+        const id = req.params.id
+        let task = readFile()
+        if (task[id]){
+            data['id']=id
+            task[id] = data
+            writeFile(task)
+            res.status(201).json({'message' :task[id]})
+    
+        } 
+        return res.json({'message' :`Task Id: ${id}not found `})
+    
+        
+    }
+
+    const remove = (req ,res) => {
+
+        const idTask=req.params.id
+        const task= readFile()[idTask]
+    
+        if(!task){
+           return  res.json({'message' : `The task with ID: ${idTask} does not exist`  })
+        }
+        delete task[idTask]
+        res.json({'message' : `The task with ID: ${idTask} has been deleted`  })
+
+    
+    
+    }
+
+
+    module.exports ={
+        getAll , getById , Create ,update ,remove
+
+    }
+
+
+
+
+
+
