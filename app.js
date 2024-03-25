ba0c31a698fcfd08d306b858bd0ae0dd1ee8354a
shahadqh7 <shahadalsadd@gmail.com>
2024-03-25 20:59:27 +0300

    added comments

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express') // Extract express library
 const app = express() //Create an instance
-app.use(express.json())
+app.use(express.json()) //sets app JSON parsing middleware
 
-const TaskRoute= require('./routes/task')
-const controller = require('./controlleer/taskController' )
+const TaskRoute= require('./routes/task') //give accesse to task route
+const controller = require('./controlleer/taskController' ) //give accesse to task controller
 
-app.use('/task' , TaskRoute)
-app.listen(4000, console.log('listening'))
-console.log("hi innvoo")
+app.use('/task' , TaskRoute) // associate a route with a router file named TaskRoute.
+app.listen(4000, console.log('listening')) //create port for the server
